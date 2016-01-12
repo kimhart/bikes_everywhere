a4320d5e9280e2487ce9b960c38180b64634fa6d
Meredith Salguero <salguero.meredith@gmail.com>
2016-01-12 15:02:50 -0500

    adds events and makes them clickable

--- public/main.js
+++ public/main.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
 	$('#calendar').fullCalendar({
+		eventClick: function(event){
+			console.log(event.title);
+			$.ajax({
+				url: "/calendar/"+event.title,
+				type: "GET",
+				dataType: "json"
+			}).done(function(response){
+				console.log(response);
+			})
+		}
+	});
 
-    })
-})
+	var getEvents = function(){
+		console.log('getting events');
+		$.ajax({
+			url:"/calendar/events",
+			type: "GET",
+			dataType: "json"
+		}).done(function(response){
+			response.forEach(function(eventObj){
+			var bikeEvent = eventObj.event_details
+			$('#calendar').fullCalendar( 'renderEvent', bikeEvent );
+
+			})
+		})
+	}
+
+	getEvents();
+});
+
+	//  timeFormat: 'H(:mm)'
+	// $('#calendar').fullCalendar( 'renderEvent', bikeEvent );
+
+
+
